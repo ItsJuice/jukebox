0fc66c6de8c26d1c97e08a27ce1996d4c2d982e9
Liam Bennett <liam@littleowllabs.com>
2016-06-17 14:04:38 +0100

    feat(videos): Store video information in DB (#18)
    * feat(videos): Store video information in DB
    
    * fix: fix crash when video is not in db
    
    * refactor: Move video repo function to a service module
    
    * test: add basic test for model changeset, also namespace modules
    
    Using ExMachina for factories (https://github.com/thoughtbot/ex_machina)
    
    * test: Add end-to-end testing. Not currently working
    
    * chore: fix hound! IT'S WORKING
    
    * chore(setup): Add circle.yml and JUnit test formatter
    
    * chore(setup): Set versions
    
    * chore: update elixir version
    
    * chore(setup): use latest version of elixir
    
    * chore: Only use .tool-versions, remove from Circle.yml
    
    * chore: see what circle thinks is in tool-versions
    
    * chore: put .tool-versions in correct format

--- web/channels/stream_channel.ex
+++ web/channels/stream_channel.ex
@@ -1,14 +1,30 @@
 defmodule Juicebox.StreamChannel do
   use Phoenix.Channel
 
+  alias Juicebox.Repo
+  alias Juicebox.Video
+  alias Juicebox.VideoServices
+
+  intercept ["video.added"]
+
   def join("stream:" <> stream_id, _params, socket) do
     {:ok, socket}
   end
 
   def handle_in("video.added", %{"video_id" => video_id}, socket) do
     IO.puts "video.added: #{video_id}"
-    broadcast! socket, "video.added", %{video_id: video_id}
 
+    video = VideoServices.find_or_create( %{video_id: video_id} )
+
+    changeset = Video.increment_queued_count_changeset(video)
+
+    case Repo.update(changeset) do
+      {:ok, video} ->
+        broadcast! socket, "video.added", %{ video_id: video.video_id, queued_count: video.queued_count }
+      {:error, changeset} ->
+        broadcast! socket, "video.error", %{ error: changeset.errors }
+    end
+    
     {:noreply, socket}
   end
 
--- web/static/js/components/videos/video.js
+++ web/static/js/components/videos/video.js
@@ -6,11 +6,12 @@ class Video extends Component {
       <div className="video" style={{display: 'inline-block'}}>
         <iframe width="280"
                 height="157"
-                src={`http://www.youtube.com/embed/${this.props.videoId}`}
+                src={`http://www.youtube.com/embed/${this.props.video.video_id}`}
                 frameBorder="0"
                 allowFullScreen>
 
         </iframe>
+        <div>Queued count: {this.props.video.queued_count}</div>
       </div>
     );
   }
