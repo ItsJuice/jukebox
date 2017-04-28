2d073f5eac685a0943d1cf02f1b99781cdc04725
Liam Bennett <liam@littleowllabs.com>
2017-04-28 01:51:41 +0100

    feat: Rudimentary skip button
    Gives everyone the right to unilaterally skip :( Just an interim until we put in
    "vote to skip"

--- apps/juicebox_web/assets/js/videos/actions.js
+++ apps/juicebox_web/assets/js/videos/actions.js
@@ -2,6 +2,7 @@ const ADD_VIDEO = 'ADD_VIDEO';
 const QUEUE_UPDATED = 'QUEUE_UPDATED';
 const PLAYING_CHANGED = 'PLAYING_CHANGED';
 const TOGGLE_EXPANDED = 'TOGGLE_EXPANDED';
+const SKIP_PLAYING = 'SKIP_PLAYING';
 
 function addVideo({ streamId, video }) {
   return {
@@ -29,12 +30,27 @@ function toggleExpanded() {
   }
 }
 
+function skipPlaying({ streamId }) {
+  console.log(streamId);
+  return {
+    type: SKIP_PLAYING,
+    socketData: {
+      event: 'video.skip_playing',
+      payload: {
+        stream_id: streamId,
+      }
+    }
+  };
+}
+
 export {
   ADD_VIDEO,
   QUEUE_UPDATED,
   PLAYING_CHANGED,
   TOGGLE_EXPANDED,
+  SKIP_PLAYING,
   addVideo,
   queueUpdated,
+  skipPlaying,
   toggleExpanded,
 };
--- apps/juicebox_web/assets/js/videos/video-page.js
+++ apps/juicebox_web/assets/js/videos/video-page.js
@@ -2,23 +2,33 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Search from '../search/search';
 import Reactions from '../reactions/reactions';
-import { addVideo, toggleExpanded } from './actions';
+import { addVideo, toggleExpanded, skipPlaying } from './actions';
 import Video from './video';
 import VideoList from './video-list';
 import styles from './video-page.scss';
 
 class VideoPage extends Component {
   render() {
-    const { queue, playing, playingStartTime, expanded, toggleExpanded } = this.props;
+    const {
+      queue,
+      playing,
+      playingStartTime,
+      expanded,
+      toggleExpanded,
+      skipPlaying,
+      streamId,
+    } = this.props;
 
     return (
       <div className={ styles['video-page'] }>
-        <Search streamId={ this.props.streamId } />
+        <Search streamId={ streamId } />
         <div className={ styles['main-column'] }>
           <Video video={ playing }
                  playingStartTime={ playingStartTime }
                  expanded={ expanded }
-                 toggleExpanded={ toggleExpanded } />
+                 toggleExpanded={ toggleExpanded }
+                 skipPlaying={ skipPlaying }
+                 streamId={ streamId } />
           <Reactions streamId={this.props.streamId} />
         </div>
         <div className={ styles['side-column']} >
@@ -39,10 +49,11 @@ VideoPage.propTypes = {
   playingStartTime: PropTypes.number,
   expanded: PropTypes.bool,
   toggleExpanded: PropTypes.func,
+  skipPlaying: PropTypes.func,
 };
 
 function mapStateToProps( { videos }, { match: { params: { streamId } } } ) {
   return Object.assign({}, videos, { streamId });
 }
 
-export default connect(mapStateToProps, { addVideo, toggleExpanded })(VideoPage);
+export default connect(mapStateToProps, { addVideo, toggleExpanded, skipPlaying })(VideoPage);
